b7f4995d9d066d2233c51c1ee6c1a3d84fc02902
shawn <xusuyuan0208@gmail.com>
2019-12-22 14:30:16 +1300

    complete application form

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -15,6 +15,34 @@ export class UserFormComponent implements OnInit {
 
   userForm: FormGroup;
 
+  driverType: string = "part-time";
+
+  passport: File = null;
+  passportUrl: any = "../../assets/images/image-upload.png";
+  passportValid: boolean = false;
+
+  visa: File = null;
+  visaUrl: any = "../../assets/images/image-upload.png";
+  visaValid: boolean = false;
+
+  licenseFront: File = null;
+  licenseFrontUrl: any = "../../assets/images/image-upload.png";
+  licenseFrontValid: boolean = false;
+
+  licenseBack: File = null;
+  licenseBackUrl: any = "../../assets/images/image-upload.png";
+  licenseBackValid: boolean = false;
+
+  resume: File = null;
+  resumeUrl: string = "../../assets/images/file-upload.png";
+  resumeName: string = "";
+  resumeValid: boolean = false;
+
+  ownerRegister: string = "owner-info";
+
+  logo: File = null;
+  logoUrl: any = "../../assets/images/image-upload.png";
+  logoValid: boolean = false;
 
   // to store user's role and action
   // used for display different form inputs and execute different commands
@@ -93,15 +121,19 @@ export class UserFormComponent implements OnInit {
 
       case this.router.url.includes("driver-register"):
         // when a driver to sign up
-        this.userForm = this.formBuilder.group({
-          firstName: ['', Validators.required],
-          lastName: ['', Validators.required],
-          phone: ['', Validators.required],
-          email: ['', Validators.required],
-          carPlate: ['', Validators.required],
-          license: ['', Validators.required],
-          password: ['', Validators.required]
-        });
+        if(this.driverType == 'part-time'){
+          this.userForm = this.formBuilder.group({
+            email: ['', Validators.required],
+            carPlate: ['', Validators.required],
+            carInsurance: [''],
+            bankAccount: ['', Validators.required],
+            passportUpload: ['', Validators.required],
+            visaUpload: ['', Validators.required],
+            licenseFrontUpload: ['', Validators.required],
+            licenseBackUpload: ['', Validators.required]
+          });
+        }
+
         break; 
 
       case this.router.url.includes("owner-register"):
@@ -109,15 +141,207 @@ export class UserFormComponent implements OnInit {
         this.userForm = this.formBuilder.group({
           firstName: ['', Validators.required],
           lastName: ['', Validators.required],
-          phone: ['', Validators.required],
+          mobile: ['', Validators.required],
           email: ['', Validators.required],
-          company: ['', Validators.required],
-          password: ['', Validators.required]
+          deduction: ['', Validators.required],
+          logoUpload: ['', Validators.required],
+          tradingName: [''],
+          address: ['', Validators.required],
+          suburb: ['', Validators.required],
+          city: ['', Validators.required],
+          postcode: ['', Validators.required],
+          contactNumber: ['', Validators.required],
+          bankAccountName: ['', Validators.required],
+          bankAccountNumber: ['', Validators.required],
+          gst: ['', Validators.required],
+          operationHour: ['', Validators.required]
         });
+
         break; 
 
     }
 
+  }
+
+  clearFileUpload(){
+    this.passport = null;
+    this.passportUrl = "../../assets/images/image-upload.png";
+    this.passportValid = false;
+
+    this.visa = null;
+    this.visaUrl = "../../assets/images/image-upload.png";
+    this.visaValid = false;
+
+    this.licenseFront = null;
+    this.licenseFrontUrl = "../../assets/images/image-upload.png";
+    this.licenseFrontValid = false;
+
+    this.licenseBack = null;
+    this.licenseBackUrl = "../../assets/images/image-upload.png";
+    this.licenseBackValid = false;
+
+    this.resume = null;
+    this.resumeUrl = "../../assets/images/file-upload.png";
+    this.resumeName = "";
+    this.resumeValid = false;    
+  }
+
+  driverTypeChange(){
+    if(this.driverType == "part-time"){
+      this.driverType = "full-time";
+      //change form build
+      this.userForm = this.formBuilder.group({
+        email: ['', Validators.required],
+        bankAccount: ['', Validators.required],
+        ird: ['', Validators.required],
+        resumeUpload: ['', Validators.required],
+        passportUpload: ['', Validators.required],
+        visaUpload: ['', Validators.required],
+        licenseFrontUpload: ['', Validators.required],
+        licenseBackUpload: ['', Validators.required]
+      });
+
+      this.clearFileUpload();
+
+    }else{
+      this.driverType = "part-time";
+      //change form build
+      this.userForm = this.formBuilder.group({
+        email: ['', Validators.required],
+        carPlate: ['', Validators.required],
+        carInsurance: [''],
+        bankAccount: ['', Validators.required],
+        passportUpload: ['', Validators.required],
+        visaUpload: ['', Validators.required],
+        licenseFrontUpload: ['', Validators.required],
+        licenseBackUpload: ['', Validators.required]
+      });
+
+      this.clearFileUpload();
+    }
+  }
+
+  fileProgress(fileInput: any, fileField: string){
+    switch(fileField){
+      case 'passport':
+        this.passport = <File>fileInput.target.files[0];
+        //show preivew
+        if(this.passport.type.match(/image\/*/) == null){
+          this.passportValid = false;
+          this.passportUrl = "../../assets/images/image-upload.png";
+          return;
+        }else{
+          let reader = new FileReader();
+          reader.readAsDataURL(this.passport);
+          reader.onload = (_event) => { 
+            this.passportUrl = reader.result; 
+          }
+          this.passportValid = true;
+        }
+
+        break;
+
+      case 'visa':
+        this.visa = <File>fileInput.target.files[0];
+        //show preview
+        if(this.visa.type.match(/image\/*/) == null){
+          this.visaValid = false;
+          this.visaUrl = "../../assets/images/image-upload.png";
+          return;
+        }else{
+          let reader = new FileReader();
+          reader.readAsDataURL(this.visa);
+          reader.onload = (_event) => {
+            this.visaUrl = reader.result;
+          }
+          this.visaValid = true;
+        }
+
+        break;
+
+      case 'licenseFront':
+        this.licenseFront = <File>fileInput.target.files[0];
+        //show preview
+        if(this.licenseFront.type.match(/image\/*/) == null){
+          this.licenseFrontValid = false;
+          this.licenseFrontUrl = "../../assets/images/image-upload.png";
+          return
+        }else{
+          let reader = new FileReader();
+          reader.readAsDataURL(this.licenseFront);
+          reader.onload = (_event) => {
+            this.licenseFrontUrl = reader.result;
+          }
+          this.licenseFrontValid = true;
+        }
+
+        break
+
+      case 'licenseBack':
+        this.licenseBack = <File>fileInput.target.files[0];
+        //show preview
+        if(this.licenseBack.type.match(/image\/*/) == null){
+          this.licenseBackValid = false;
+          this.licenseBackUrl = "../../assets/images/image-upload.png";
+          return
+        }else{
+          let reader = new FileReader();
+          reader.readAsDataURL(this.licenseBack);
+          reader.onload = (_event) => {
+            this.licenseBackUrl = reader.result;
+          }
+          this.licenseBackValid = true;
+        }
+
+        break
+
+      case 'resume':
+        let validTypes = ['doc', 'pdf', 'txt', 'odf'];
+        this.resume = <File>fileInput.target.files[0];
+
+        //show file name
+        let nameArray = this.resume.name.split('.');
+        if(validTypes.includes(nameArray[nameArray.length - 1].toLowerCase())){
+          this.resumeValid = true;
+          this.resumeName = this.resume.name;
+          this.resumeUrl = "../../assets/images/file.png";
+        }else{
+          this.resumeValid = false;
+          this.resumeUrl = "../../assets/images/file-upload.png";
+        }
+
+        break;
+
+      case 'logo':
+        this.logo = <File>fileInput.target.files[0];
+        let validLogoTypes = ['jpg', 'pdf', 'png'];
+        let logoNameArray = this.logo.name.split('.');
+
+        //show preview
+        if(this.logo.type.match(/image\/*/) == null || !validLogoTypes.includes(logoNameArray[logoNameArray.length - 1].toLowerCase())){
+          this.logoValid = false;
+          this.licenseBackUrl = "../../assets/images/image-upload.png";
+          return
+        }else{
+          let reader = new FileReader();
+          reader.readAsDataURL(this.logo);
+          reader.onload = (_event) => {
+            this.logoUrl = reader.result;
+          }
+          this.logoValid = true;
+        }
+
+        break
+
+    }
+  }
+
+  ownerRegisterPageChange(){
+    if(this.ownerRegister == 'owner-info'){
+      this.ownerRegister = 'company-info';
+    }else{
+      this.ownerRegister = 'owner-info';
+    }
   }
 
 
